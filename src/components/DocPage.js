0e4515fec7d20260c82989a9a74320d82ad60f74
Matthew Phillips <matthew@phillipsoft.com>
2021-12-23 16:39:19 -0500

    Upgrade astro

---
+++ src/components/DocPage.js
@@ -0,0 +1,7 @@
+import * as info from '../info.js';
+
+export { info };
+export { default as Section } from './DocSection.astro';
+export { default as Block } from './DocBlock.astro';
+export { default as Code } from './Code.astro';
+export { default as Page } from './DocPage.astro';
