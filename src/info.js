4ab3ac29395c199e6208fa559e65122fecbe70b6
Matthew Phillips <matthew@phillipsoft.com>
2021-12-27 08:35:54 -0500

    Update corset version

--- src/info.js
+++ src/info.js
@@ -1,8 +1,8 @@
 
 export const project = 'Corset';
 
-const CDNURL = version => `https://cdn.corset.dev/${version}/dsl.js`;
+const CDNURL = version => `https://cdn.corset.dev/${version}/main.js`;
 
-export const version = '0.1.4';
+export const version = '0.2.0';
 export const cdnURL = CDNURL(version);
 export const npmPkg = 'corset';
