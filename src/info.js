245afc5514bf7f75a139f17ca0760e64d21c8a5a
Matthew Phillips <matthew@phillipsoft.com>
2022-01-08 10:48:46 -0500

    Use Corset 0.4.0

--- src/info.js
+++ src/info.js
@@ -7,7 +7,7 @@ export const year = new Date().getFullYear();
 const CDNURL = version => `https://cdn.corset.dev/${version}/main.js`;
 const CDNWASMURL = version => `https://cdn.corset.dev/${version}/main.wasm`;
 
-export const version = '0.3.1';
+export const version = '0.4.0';
 export const cdnURL = CDNURL(version);
 export const cdnWasmURL = CDNWASMURL(version);
 export const npmPkg = 'corset';
