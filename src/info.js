ec8eb1afcda56d3d9f8bb1b52c53da9e6242de27
Matthew Phillips <matthew@phillipsoft.com>
2022-03-08 07:22:16 -0500

    Use 1.0.0

--- src/info.js
+++ src/info.js
@@ -8,8 +8,8 @@ const CDNURL = major => `https://cdn.corset.dev/${major}`;
 const CDNURL_EXACT = version => `https://cdn.corset.dev/${version}/main.js`;
 const CDNWASMURL = version => `https://cdn.corset.dev/${version}/main.wasm`;
 
-export const major = 'v0';
-export const version = '0.9.3';
+export const major = 'v1';
+export const version = '1.0.0';
 export const cdnURL = CDNURL(major);
 export const cdnURLExact = CDNURL_EXACT(version);
 export const cdnWasmURL = CDNWASMURL(version);
